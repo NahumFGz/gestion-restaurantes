cb24f8ee57543d8a17bf811e52dcd3084f74c2fd
NahumFG <ngutierrez2507@gmail.com>
2024-05-06 16:58:18 -0500

    frontend: formulario de inicio de sesión terminado

---
+++ frontend/src/components/admin/LoginForm.jsx
@@ -0,0 +1,64 @@
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+function initialValues () {
+  return {
+    email: '',
+    password: ''
+  }
+}
+
+function validationSchema () {
+  return Yup.object({
+    email: Yup.string().email('Correo electrónico no válido').required('El correo electrónico es obligatorio'),
+    password: Yup.string().required('La contraseña es obligatoria')
+  })
+}
+
+export function LoginForm () {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    onSubmit: (formValues) => {
+      console.log('Formulario enviado')
+      console.log(formValues)
+    }
+  })
+
+  return (
+    <div>
+      <form className='flex flex-col' onSubmit={formik.handleSubmit}>
+        <input
+          className='border border-gray-300 p-2 mb-2 rounded-md'
+          name='email' type='text' placeholder='Correo electrónico'
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.email && formik.errors.email
+          ? (
+            <div className='text-red-500'>{formik.errors.email}</div>
+            )
+          : null}
+        <input
+          className='border border-gray-300 p-2 mb-2 rounded-md'
+          name='password' type='password' placeholder='Password'
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        {
+            (formik.touched.password && formik.errors.password) &&
+            (<div className='text-red-500'>{formik.errors.password}</div>)
+        }
+
+        <button
+          className='bg-blue-500 text-white p-2 rounded-md'
+          type='submit'
+        >
+          Iniciar sesión
+        </button>
+      </form>
+    </div>
+
+  )
+}
--- frontend/src/pages/admin/LoginAdmin.jsx
+++ frontend/src/pages/admin/LoginAdmin.jsx
@@ -1,9 +1,13 @@
+import { LoginForm } from '../../components'
+
 export function LoginAdmin () {
   return (
     <div className='h-screen w-full flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500'>
-      <div className='bg-white w-[500px] p-2 border rounded-md shadow-lg shadow-red-500/50'>
-        <h1 className='p-10 mb-10 font-bold text-3xl text-center'>Entrar al panel</h1>
-        <p>Formulario</p>
+      <div className='p-10 bg-white w-[500px] border rounded-md shadow-lg shadow-red-500/50'>
+        <h1 className='mb-10 font-bold text-3xl text-center'>
+          Entrar al panel
+        </h1>
+        <LoginForm />
       </div>
     </div>
 
