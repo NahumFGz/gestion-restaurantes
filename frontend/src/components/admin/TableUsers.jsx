b1de83c499b0365727225105adb238778a1f3ab8
NahumFG <ngutierrez2507@gmail.com>
2024-05-08 16:57:00 -0500

    frontend: Agregando acciones a la tabla de usuarios

--- frontend/src/components/admin/TableUsers.jsx
+++ frontend/src/components/admin/TableUsers.jsx
@@ -1,30 +1,61 @@
+import { CheckBadgeIcon, XMarkIcon } from '@heroicons/react/24/solid'
+
 export function TableUsers ({ users }) {
   return (
-    <>
-      <div className='overflow-auto shadow-lg rounded-lg'>
-        <table className='w-full table-auto bg-white rounded-lg border border-gray-200'>
-          <thead className='bg-gray-800 text-white'>
-            <tr>
-              <th className='px-6 py-4 text-left'>Username</th>
-              <th className='px-6 py-4 text-left'>Nombre</th>
-              <th className='px-6 py-4 text-left'>Correo</th>
-              <th className='px-6 py-4 text-left'>Activo</th>
-              <th className='px-6 py-4 text-left'>Staff</th>
+    <div className='overflow-auto shadow-lg rounded-lg'>
+      <table className='w-full table-auto bg-white rounded-lg border border-gray-200'>
+        <thead className='bg-gray-800 text-white'>
+          <tr>
+            <th className='px-6 py-4 text-left'>Username</th>
+            <th className='px-6 py-4 text-left'>Nombre</th>
+            <th className='px-6 py-4 text-left'>Correo</th>
+            <th className='px-6 py-4 text-left'>Activo</th>
+            <th className='px-6 py-4 text-left'>Staff</th>
+            <th className='px-6 py-4 text-left'>Acciones</th>
+          </tr>
+        </thead>
+        <tbody className='text-gray-700'>
+          {users.map((user) => (
+            <tr key={user.id} className='border-b border-gray-200 hover:bg-gray-100'>
+              <td className='px-6 py-4'>{user.username}</td>
+              <td className='px-6 py-4'>{`${user.first_name} - ${user.last_name}`}</td>
+              <td className='px-6 py-4'>{user.email}</td>
+              <td className='px-6 py-4'>
+                {user.is_active
+                  ? (
+                    <CheckBadgeIcon className='h-5 w-5 text-green-500' />
+                    )
+                  : (
+                    <XMarkIcon className='h-5 w-5 text-red-500' />
+                    )}
+              </td>
+              <td className='px-6 py-4'>
+                {user.is_staff
+                  ? (
+                    <CheckBadgeIcon className='h-5 w-5 text-green-500' />
+                    )
+                  : (
+                    <XMarkIcon className='h-5 w-5 text-red-500' />
+                    )}
+              </td>
+              <td className='flex text-sm justify-start gap-2 px-6 py-4'>
+                <button
+                  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                  onClick={() => console.log('Editar:', user.id)}
+                >
+                  Editar
+                </button>
+                <button
+                  className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+                  onClick={() => console.log('Eliminar:', user.id)}
+                >
+                  Eliminar
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody className='text-gray-700'>
-            {users.map((user) => (
-              <tr key={user.id} className='border-b border-gray-200 hover:bg-gray-100'>
-                <td className='px-6 py-4'>{user.username}</td>
-                <td className='px-6 py-4'>{`${user.first_name} - ${user.last_name}`}</td>
-                <td className='px-6 py-4'>{user.email}</td>
-                <td className='px-6 py-4'>{user.is_active ? 'Sí' : 'No'}</td>
-                <td className='px-6 py-4'>{user.is_staff ? 'Sí' : 'No'}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
