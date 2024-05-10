82697e1516f8dd248964b3b396e88f73c10e6068
NahumFG <ngutierrez2507@gmail.com>
2024-05-10 06:58:21 -0500

    backend: agregando datos de categoría producto

--- backend/products/api/serializers.py
+++ backend/products/api/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 
+from categories.api.serializers import CategorySerializer
 from products.models import Product
 
 
 class ProductSerializer(ModelSerializer):
+    categoty_data = CategorySerializer(source="category", read_only=True)
+
     class Meta:
         model = Product
-        fields = ("id", "title", "image", "price", "active", "category")
+        fields = (
+            "id",
+            "title",
+            "image",
+            "price",
+            "active",
+            "category",
+            "categoty_data",
+        )
