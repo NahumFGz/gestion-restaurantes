59f6f844492eb02de1c3bb25316b05449609cfa4
NahumFG <ngutierrez2507@gmail.com>
2024-05-10 09:40:48 -0500

    frontend: formulario para crear nuevo producto

---
+++ frontend/src/components/admin/AddEditProductForm.jsx
@@ -0,0 +1,91 @@
+import { useId } from 'react'
+
+export function AddEditProductForm () {
+  const productId = useId()
+  const priceId = useId()
+  const categoryId = useId()
+  const activeId = useId()
+  const imageId = useId()
+
+  return (
+    <form
+      className='space-y-6'
+    >
+      <div className='flex flex-col gap-10'>
+        <div>
+          <label htmlFor={productId} className='block text-sm font-medium text-gray-700'>
+            Producto
+          </label>
+          <input
+            className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+            id={productId} name='product' type='text'
+          />
+        </div>
+        <div>
+          <label htmlFor={priceId} className='block text-sm font-medium text-gray-700'>
+            Precio
+          </label>
+          <input
+            className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+            id={priceId} name='price' type='number'
+          />
+        </div>
+        <div>
+          <label htmlFor={categoryId} className='block text-sm font-medium text-gray-700'>
+            Categoria
+          </label>
+          <select
+            className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+            id={categoryId} name='categoria'
+          >
+            <option value=''>Selecciona una categoría</option>
+            <option value='categoria1'>Categoria 1</option>
+            <option value='categoria2'>Categoria 2</option>
+            <option value='categoria3'>Categoria 3</option>
+            {/* Agrega más opciones aquí según sea necesario */}
+          </select>
+        </div>
+        <div className='flex items-center'>
+          <input
+            className='h-4 w-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500'
+            id={activeId} name='is_active' type='checkbox'
+          />
+          <label htmlFor={activeId} className='ml-2 block text-sm text-gray-700'>
+            Producto activo
+          </label>
+        </div>
+        <div>
+          <label
+            className='block text-sm font-medium text-gray-700'
+            htmlFor={imageId}
+          >
+            Imagen
+          </label>
+          <input
+            className='hidden'
+            id={imageId}
+            name='image'
+            type='file'
+            accept='.png, .jpg, .jpeg'
+          />
+          <button
+            type='button'
+            className='mt-1 block w-full border border-dashed border-indigo-500 rounded-md bg-white py-2 px-4 text-sm font-medium text-indigo-700 hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+          >
+            Cargar imagen
+          </button>
+        </div>
+
+      </div>
+      <div className='mt-4'>
+        <button
+          className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+          type='submit'
+        >
+          'Actualizar'
+        </button>
+      </div>
+    </form>
+
+  )
+}
--- frontend/src/pages/admin/ProductsAdmin.jsx
+++ frontend/src/pages/admin/ProductsAdmin.jsx
@@ -1,19 +1,37 @@
-import { useEffect } from 'react'
-import { HeaderPage, Loading, TableProducts } from '../../components'
+import { useEffect, useState } from 'react'
+import { AddEditProductForm, HeaderPage, Loading, ModalBasic, TableProducts } from '../../components'
 import { useProducts } from '../../hooks'
 
 export function ProducsAdmin () {
   const { products, loading, getProducts } = useProducts()
+  const [showModal, setShowModal] = useState(false)
+  const [titleModal, setTitleModal] = useState(null)
+  const [contentModal, setContentModal] = useState(null)
 
   useEffect(() => { getProducts() }, [])
-  console.log(products)
+
+  const openCloseModal = () => { setShowModal((prev) => !prev) }
+
+  const addProduct = () => {
+    setTitleModal('Nuevo producto')
+    setContentModal(<AddEditProductForm onClose={openCloseModal} />)
+    openCloseModal()
+  }
 
   return (
     <>
-      <HeaderPage title='Productos' btnTitle='Nuevo producto' />
+      <HeaderPage title='Productos' btnTitle='Nuevo producto' btnClick={addProduct} />
       {loading
         ? <Loading />
         : <TableProducts products={products} />}
+
+      <ModalBasic
+        title={titleModal}
+        show={showModal}
+        onClose={openCloseModal}
+      >
+        {contentModal}
+      </ModalBasic>
     </>
   )
 }
