c69f1d0a27cac903c7a5b4a0d98bde36302078ee
NahumFG <ngutierrez2507@gmail.com>
2024-05-09 23:57:21 -0500

    backend: Agregando filtros por categoria

--- backend/products/api/views.py
+++ backend/products/api/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
@@ -10,3 +11,5 @@ class ProductApiViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
     # Filters ...
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["active", "category"]
