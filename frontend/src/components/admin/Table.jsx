25de78bc41226c8246fae97839a8fbe4b13102c0
NahumFG <ngutierrez2507@gmail.com>
2024-05-12 00:27:33 -0500

    frontend: estilos a pedidos

--- frontend/src/components/admin/Table.jsx
+++ frontend/src/components/admin/Table.jsx
@@ -1,17 +1,28 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import icon from '../../assets/table.svg'
 import { getOrdersByTableApi } from '../../api/orders'
 import { ORDER_STATUS } from '../../utils/constants'
 
 export function Table ({ table }) {
+  const [orders, setOrders] = useState([])
+
   useEffect(() => {
-    getOrdersByTableApi(table.id, ORDER_STATUS.PENDING)
-      .then(response => console.log('response ORDER_STATUS', table.number, response))
-      .catch(error => console.error(error))
+    const fetchOrders = async () => {
+      const response = await getOrdersByTableApi(table.id, ORDER_STATUS.PENDING)
+      console.log('response ORDER_STATUS', table.number, response)
+      setOrders(response)
+    }
+    fetchOrders()
   }, [])
 
   return (
     <div className='flex justify-center items-center hover:scale-95 active:scale-105'>
+      {Object.keys(orders).length > 0 && (
+        <div className='fixed bg-red-500 text-white text-xs font-bold rounded-full px-4'>
+          {Object.keys(orders).length}
+        </div>
+      )}
+
       <div>
         <img
           className='w-56 h-56'
