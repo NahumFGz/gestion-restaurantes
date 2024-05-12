3cdeaf9234e358d9adf18e94f7be6cc57daa881e
NahumFG <ngutierrez2507@gmail.com>
2024-05-11 21:57:01 -0500

    backend: Agregando filtrado en la API

--- backend/orders/api/views.py
+++ backend/orders/api/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from orders.api.serializers import OrderSerializer
@@ -7,3 +9,6 @@ from orders.models import Order
 class OrderApiViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["table", "status", "close"]
+    ordering_fields = "__all__"
