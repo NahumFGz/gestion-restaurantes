a2015dfb5f62cb74586bdd7c3547496d2b4ab159
NahumFG <ngutierrez2507@gmail.com>
2024-05-11 21:31:59 -0500

    backend: modelo de Orders

--- backend/api_icard/settings.py
+++ backend/api_icard/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "categories",
     "products",
     "tables",
+    "orders",
 ]
 
 MIDDLEWARE = [
---
+++ backend/orders/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+status_choices = (
+    ("PENDING", "pending"),
+    ("DELIVERED", "delivered"),
+)
+
+
+# Create your models here.
+class Order(models.Model):
+    table = models.ForeignKey(
+        "tables.Table", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    product = models.ForeignKey(
+        "products.Product", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    status = models.CharField(max_length=50, choices=status_choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    close = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.table)
