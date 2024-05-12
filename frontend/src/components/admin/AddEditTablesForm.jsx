d164aa8ca758776fcb4d04905f0660272aad4e41
NahumFG <ngutierrez2507@gmail.com>
2024-05-11 19:33:10 -0500

    frontend: formulario y validación de mesas

---
+++ frontend/src/components/admin/AddEditTablesForm.jsx
@@ -0,0 +1,51 @@
+import { useFormik } from 'formik'
+import { useId } from 'react'
+import * as Yup from 'yup'
+
+export function AddEditTablesForm ({ onClose }) {
+  const idNumber = useId()
+
+  const formik = useFormik({
+    initialValues: {
+      number: ''
+    },
+    validationSchema: Yup.object({
+      number: Yup.string().required('El número de mesa es obligatorio')
+    }),
+    onSubmit: async (formValues) => {
+      console.log('Formulario enviado:', formValues)
+    }
+  })
+
+  return (
+    <form onSubmit={formik.handleSubmit} className='space-y-6'>
+      <div className='flex flex-col gap-10'>
+        <div>
+          <label htmlFor={idNumber} className='block text-sm font-medium text-gray-700'>
+            Numero de mesa
+          </label>
+          <input
+            className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+            id={idNumber}
+            name='number'
+            type='text'
+            value={formik.values.number}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.number && formik.errors.number && (
+            <div className='text-red-500 text-sm'>{formik.errors.number}</div>
+          )}
+        </div>
+      </div>
+      <div className='mt-4'>
+        <button
+          className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+          type='submit'
+        >
+          Crear
+        </button>
+      </div>
+    </form>
+  )
+}
--- frontend/src/pages/admin/TablesAdmin.jsx
+++ frontend/src/pages/admin/TablesAdmin.jsx
@@ -1,19 +1,39 @@
-import { useEffect } from 'react'
-import { HeaderPage, Loading, TableTables } from '../../components'
+import { useEffect, useState } from 'react'
+import { AddEditTablesForm, HeaderPage, Loading, ModalBasic, TableTables } from '../../components'
 import { useTables } from '../../hooks'
 
 export default function TablesAdmin () {
   const { loading, tables, getTables } = useTables()
+  const [showModal, setShowModal] = useState(false)
+  const [titleModal, setTitleModal] = useState('')
+  const [modalContent, setModalContent] = useState(null)
 
   useEffect(() => { getTables() }, [])
+
+  const openCloseModal = () => setShowModal((prevState) => !prevState)
+
+  const addTable = () => {
+    setTitleModal('Crear nueva mesa')
+    setModalContent(<AddEditTablesForm />)
+    openCloseModal()
+  }
+
   return (
     <>
-      <HeaderPage title='Mesas' btnTitle='Crear nueva mesa' />
+      <HeaderPage title='Mesas' btnTitle='Crear nueva mesa' btnClick={addTable} />
       {
         loading
           ? <Loading />
           : <TableTables tables={tables} />
       }
+
+      <ModalBasic
+        title={titleModal}
+        show={showModal}
+        onClose={openCloseModal}
+      >
+        {modalContent}
+      </ModalBasic>
     </>
   )
 }
