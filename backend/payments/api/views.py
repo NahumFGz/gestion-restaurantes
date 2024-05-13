d61363838d5187777a92d61deac47265913270cd
NahumFG <ngutierrez2507@gmail.com>
2024-05-13 15:58:14 -0500

    backend: sistema de filtros de payments

--- backend/payments/api/views.py
+++ backend/payments/api/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
 
 from payments.api.serializers import PaymentSerializer
@@ -8,4 +10,7 @@ from payments.models import Payment
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = ["table", "status_payment"]
+    ordering_fields = "__all__"
+    search_fields = ["table", "status_payment"]
